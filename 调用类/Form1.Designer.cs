30b825a6bbdb62a3853e38b9fb7355df54d07fc6
suzyuhug <Yufei.huang@flextronics.com>
2016-12-15 08:07:41 +0800

    再次提交

--- 调用类/Form1.Designer.cs
+++ 调用类/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(118, 81);
+            this.button1.Location = new System.Drawing.Point(149, 82);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 40);
             this.button1.TabIndex = 0;
